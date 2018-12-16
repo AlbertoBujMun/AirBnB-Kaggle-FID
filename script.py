1dba039c7a4a559b0af694127b7223393b4094d3
Belen Ramos <belramgut@alum.us.es>
2018-12-16 17:02:24 +0100

    cleaning outliers in age distribution

--- script.py
+++ script.py
@@ -59,3 +59,39 @@ print("Gender percentage 2011: \n", gender_percentage2011, "\n")
 print("Gender percentage 2012: \n", gender_percentage2012, "\n")
 print("Gender percentage 2013: \n", gender_percentage2013, "\n")
 print("Gender percentage 2014: \n", gender_percentage2014, "\n")
+
+# ------------------------ Destination study by years ------------------------
+x2010_values = data.country_destination[data.date_account_created == 2010].value_counts()
+x2011_values = data.country_destination[data.date_account_created == 2011].value_counts()
+x2012_values = data.country_destination[data.date_account_created == 2012].value_counts()
+x2013_values = data.country_destination[data.date_account_created == 2013].value_counts()
+x2014_values = data.country_destination[data.date_account_created == 2014].value_counts()
+
+# ------------------------ Distribution by age per year ------------------------
+x2010 = data[data.date_account_created == 2010]
+x2011 = data[data.date_account_created == 2011]
+x2012 = data[data.date_account_created == 2012]
+x2013 = data[data.date_account_created == 2013]
+x2014 = data[data.date_account_created == 2014]
+
+plt.figure()
+plt.boxplot([x2010.age, x2011.age, x2012.age, x2013.age, x2014.age])
+plt.savefig('charts and diagrams/agesdistribution.png')
+
+# ------------------------ Removing the upper outliers ------------------------
+
+p1 = np.percentile(x2010.age, [5, 95])
+p2 = np.percentile(x2011.age, [5, 95])
+p3 = np.percentile(x2012.age, [5, 95])
+p4 = np.percentile(x2013.age, [5, 95])
+p5 = np.percentile(x2014.age, [5, 95])
+
+new_df1 = x2010[(x2010.age > p1[0]) & (x2010.age < p1[1])]
+new_df2 = x2011[(x2011.age > p2[0]) & (x2011.age < p2[1])]
+new_df3 = x2012[(x2012.age > p3[0]) & (x2012.age < p3[1])]
+new_df4 = x2013[(x2013.age > p4[0]) & (x2013.age < p4[1])]
+new_df5 = x2014[(x2014.age > p5[0]) & (x2014.age < p5[1])]
+
+plt.figure()
+plt.boxplot([new_df1.age, new_df2.age, new_df3.age, new_df4.age, new_df5.age])
+plt.savefig('charts and diagrams/agesdistribution_without_outliers.png')
